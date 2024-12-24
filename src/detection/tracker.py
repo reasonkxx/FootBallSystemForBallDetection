e322cc5966372d424946903928492c553ddd2eb5
reasonkxx <misha.chubko@gmail.com>
2024-12-24 11:41:27 +0100

    Remove is_referee function

--- src/detection/tracker.py
+++ src/detection/tracker.py
@@ -10,13 +10,14 @@ import sys
 
 
 class BallTracker:
+
     def __init__(self, max_disappeared=10):
         self.last_position = None
         self.disappeared = 0
         self.max_disappeared = max_disappeared
 
     def update(self, detections):
-        # Проверяем, найдены ли объекты
+        # Перевіряємо чи знайден м'яч
         ball_detected = False
         for detection in detections:
             for box in detection.boxes:
@@ -29,26 +30,17 @@ class BallTracker:
     
     
 
-        # Если мяча не обнаружено, увеличиваем счетчик исчезновений
+        # Якщо м'яч не знайдено, то увеличим лічильник
         if not ball_detected:
             self.disappeared += 1
             if self.disappeared <= self.max_disappeared and self.last_position is not None:
-                return self.last_position  # Возвращаем последнюю известную позицию
+                return self.last_position  # Повертаємо останню відому позицію
             else:
-                self.last_position = None  # Сбрасываем, если мяч долго отсутствует
+                self.last_position = None  # Відновлюємо, якщо м'яч довго відстутній
+        return None
+    
+    def some():
         return None
         
-    def is_referee(frame, x1, y1, x2, y2):
-        # Вирізаємо область рамки
-        roi = frame[y1:y2, x1:x2]
-        # Перетворюємо область у HSV
-        hsv_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
-        # Визначаємо діапазон кольорів для форми рефері (наприклад, чорний)
-        lower_black = np.array([0, 0, 0])
-        upper_black = np.array([180, 255, 50])
-        # Створюємо маску для чорного кольору
-        mask = cv2.inRange(hsv_roi, lower_black, upper_black)
-        # Розраховуємо відсоток чорного кольору
-        black_ratio = np.sum(mask > 0) / (roi.shape[0] * roi.shape[1])
-        return black_ratio > 0.5  # Якщо більше 50% чорного, то це рефері
+        
    
